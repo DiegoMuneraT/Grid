31d029c84b256229b2a1e2888418c8e36e5370ee
DanyMelguizo <dmelguizor@eafit.edu.co>
2023-09-13 15:31:19 -0500

    Frontend mapa

--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -3,8 +3,11 @@ import NavBarApp from "../components/NavBarApp";
 export default function Map() {
   return (
     <body>
+      <div style={{position: 'absolute', right: '0', width: '15rem', height: '5rem', backdropFilter: 'opacity(1) blur(0px)', opacity: '1', padding: '15px', background: 'var(--bs-body-color)', color: 'var(--bs-emphasis-color)',}}>
+        <p style={{color: 'var(--bs-body-bg)', marginBottom: '0px', opacity: '1',}}>Vehiculo</p>
+        <p style={{color: 'var(--bs-body-bg)', marginBottom: '0px', opacity: '1',}}>Placa</p>
+      </div>
       <NavBarApp/>
-        
       <div style={{height: '100vh', overflowY: 'hidden'}}><iframe title="map" allowFullScreen="" src="https://cdn.bootstrapstudio.io/placeholders/map.html" style={{width: '100%', height: '100%', position: 'static',}}></iframe></div>
     </body>
   )
