1ae246ece1f842ec6808c87f138ddbbd52623be5
DanyMelguizo <dmelguizor@eafit.edu.co>
2023-09-17 19:08:38 -0500

    base de datos

--- application/models.py
+++ application/models.py
@@ -15,6 +15,9 @@ class Vehicle(models.Model):
     usuario = models.CharField(max_length=100)
     activo = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.placa
+
 
 class Station(models.Model):
     id = models.AutoField(primary_key=True)
@@ -24,6 +27,9 @@ class Station(models.Model):
     elevation = models.FloatField()
     charger_types = models.JSONField()
     number_of_chargers = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class Operation(models.Model):
