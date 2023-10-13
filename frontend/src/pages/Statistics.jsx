e1c510fc379426ab919848fbae372e5d09f61e26
DanyMelguizo <dmelguizor@eafit.edu.co>
2023-10-13 15:04:00 -0500

    actualizar vehiculos, no funciona aun

--- frontend/src/pages/Statistics.jsx
+++ frontend/src/pages/Statistics.jsx
@@ -4,15 +4,19 @@ import BatteryVoltage from "components/grafics/battery/batteryVoltage";
 const Statistics = () => {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Estadísticas</h1>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ flex: 1 }}>
-          <BatteryConsumption />
+      <body style={{background: '#f9f9f9'}}>
+        <h1 style={{ textAlign: "center"}}>Estadísticas</h1>
+
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ flex: 1 }}>
+            <BatteryConsumption />
+          </div>
+          <div style={{ flex: 1 }}>
+            <BatteryVoltage />
+          </div>
         </div>
-        <div style={{ flex: 1 }}>
-          <BatteryVoltage />
-        </div>
-      </div>
+      </body>
+
     </>
   );
 };
