2fe7a2b7ae811de7abc3e8c760cc8686ef3ffcbe
BrigithGiraldo <brigithgiraldo2002@gmal.com>
2023-10-24 01:45:57 -0500

    Voltaje con datos reales

--- frontend/src/api/operationServer.js
+++ frontend/src/api/operationServer.js
@@ -9,3 +9,23 @@ const operationApi = axios.create({
 export const createOperation = async (newOperation) => {
     return await operationApi.post("/", newOperation)
 }
+
+//obtener Voltaje
+export const getVoltageDataForVehicle = async (vehicleId) => {
+  try {
+    const response = await operationApi.get('/'); 
+    const allData = response.data;
+
+    // Filtrar los datos para obtener solo aquellos con el vehicle_id específico
+    const filteredData = allData.filter(entry => entry.vehicle_id === vehicleId);
+
+    const voltageData = filteredData.map(entry => ({
+      timestamp: entry.timestamp,
+      voltage: entry.voltage
+    }));
+
+    return voltageData;
+  } catch (error) {
+    throw error;
+  }
+};
--- frontend/src/components/grafics/battery/batteryVoltage.jsx
+++ frontend/src/components/grafics/battery/batteryVoltage.jsx
@@ -1,65 +1,93 @@
 import React, { useState, useEffect } from 'react';
-import { Pie } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
+import { getVoltageDataForVehicle } from "api/operationServer"; 
 
 const BatteryVoltage = () => {
+  const [voltageData, setVoltageData] = useState([]);
+  const [currentVoltage, setCurrentVoltage] = useState(0); 
 
-    const [currentVoltage, setCurrentVoltage] = useState(220); // Valor que cambia
+  useEffect(() => {
+    const vehicleId = "FXR906"; // Reemplazar con el ID del vehículo seleccionado
 
-    useEffect(() => {
-      const fetchCurrentVoltage = () => {
-        // Aquí se realiza una solicitudo para obtener el valor actual del voltaje.
-        // Por ahora, lo simulamos con un valor fijo.
-        setCurrentVoltage(220); //Valor que cambia
-      };
+    const fetchVoltageData = async () => {
+      try {
+        const data = await getVoltageDataForVehicle(vehicleId);
 
-      fetchCurrentVoltage();
-    }, []);
-  
-    const data = {
-        labels: ['Voltaje Actual', 'Voltaje Restante'],
-        datasets: [
-          {
-            data: [currentVoltage, 240 - currentVoltage],
-            backgroundColor: ['rgba(75, 192, 192, 0.6)', 'rgba(255, 99, 132, 0.6)'],
-            borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
-            borderWidth: 0,
-          },
-        ],
+        if (data && data.length > 0) {
+          const lastVoltageReading = data[data.length - 1].voltage;
+          setCurrentVoltage(lastVoltageReading);
+
+          const historicalData = data.map(entry => ({
+            x: entry.timestamp,
+            y: entry.voltage
+          }));
+
+          setVoltageData(historicalData);
+        }
+      } catch (error) {
+        console.error(error);
+      }
     };
-      
-    const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: true,
-            position: 'bottom',
-          },
-        },
-    };
-      
-  
-    return (
-      <div>
-        <section className="clean-block clean-blog-list dark" style={{ height: '100vh', overflowY: 'hidden' }}>
-          <div className="container">
-            <div className="block-content" style={{ margin: '80px 0 0 80px',  width: '650px', height: '600px' }}>
-              <h2 style={{ textAlign: 'center' }}>Voltaje Actual</h2>
-              {/* Contenedor de la gráfica */}
-              <div style={{ width: '300px', height: '300px', position: 'relative', top: '15%', left: '20%'}}>
-                {/* Componente Pie de react-chartjs-2 */}
-                <Pie data={data} options={options} />
-                {/* Mostrar el valor actual de voltaje en el centro */}
-                <div style={{ position: 'absolute', top: '50%', left: '35%'}}>
-                <h2>{currentVoltage} V</h2>
+
+    fetchVoltageData();
+  }, []);
+
+  const voltageValues = voltageData.map(entry => entry.y);
+
+  const data = {
+    labels: voltageData.map(entry => entry.x),
+    datasets: [
+      {
+        label: 'Voltaje Histórico',
+        data: voltageValues,
+        fill: false,
+        borderColor: 'rgba(75, 192, 192, 0.6)',
+        tension: 0.1,
+      },
+    ],
+  };
+
+  const yAxes = [{
+    ticks: {
+      min: 0,
+      max: 60,
+    },
+  }];
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      y: yAxes,
+    },
+  };
+
+  return (
+    <div>
+      <section className="clean-block clean-blog-list dark" style={{ height: '100vh', overflowY: 'hidden' }}>
+        <div className="container">
+          <div className="block-content" style={{ margin: '80px 0 0 80px', width: '650px', height: '600px' }}>
+            <h2 style={{ textAlign: 'center' }}>Voltaje Histórico</h2>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div style={{ width: '80%', height: '300px', position: 'relative', top: '10%', left: '0%' }}>
+                <Line data={data} options={options} />
+              </div>
+              <div style={{ width: '20%', height: '100px', display: 'flex', flexDirection: 'column', justifyContent: 'center', background: 'lightgreen', padding: '10px' }}>
+                <div style={{ textAlign: 'center' }}>
+                  <h5 style={{ margin: '0', padding: '0' }}>{currentVoltage} Voltaje actual</h5>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    );
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default BatteryVoltage;
-
