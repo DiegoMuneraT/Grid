ab7256099f1a882e150ba9074f7ea39b32f6367e
DanyMelguizo <dmelguizor@eafit.edu.co>
2023-11-20 01:24:46 -0500

    recomendaciones

--- frontend/src/components/Comentarios.js
+++ frontend/src/components/Comentarios.js
@@ -8,7 +8,7 @@ function Comentarios({title, date, user, description}) {
         <div className="col-lg-5"><img className="rounded img-fluid" src={image} style={{height: '250px', width: '90%'}}/></div>
         <div className="col-lg-7">
           <h3>{title}</h3>
-          <div className="info"><span className="text-muted">{date} por&nbsp;<a href="#">{user}</a></span></div>
+          <div className="info"><span className="text-muted">{date} por</span><b>{user}</b></div>
             <p>{description}</p>
           </div>
       </div>
